78b185f0fa81598ea007bb11b761d812db4ba299
SegaChuang <chuang.sega@iac.com.tw>
2026-01-16 16:47:57 +0800

    Update members.ts file

--- src/services/members.ts
+++ src/services/members.ts
@@ -2,18 +2,21 @@ import { api } from './api'
 
 export type Member = { id: string; name: string; email: string; role: string }
 
-export const getMembers = async () => {
+export const getMembers = async (): Promise<Member[]> => {
   const res = await api.get<Member[]>('/members')
   return res.data
 }
+
 export const createMember = async (p: Omit<Member, 'id'>) => {
   const res = await api.post<Member>('/members', p)
   return res.data
 }
+
 export const updateMember = async (id: string, p: Partial<Member>) => {
   const res = await api.put<Member>(`/members/${id}`, p)
   return res.data
 }
+
 export const deleteMember = async (id: string) => {
   const res = await api.delete(`/members/${id}`)
   return res.data
